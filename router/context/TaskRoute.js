ce0f8e42094009de512ed55252060f9de0f9e993
bagustri3 <tribagus0510@gmail.com>
2023-03-06 14:19:36 +0700

    task updated

--- router/context/TaskRoute.js
+++ router/context/TaskRoute.js
@@ -5,7 +5,7 @@ const task = require("express").Router();
 task.post("/", TaskController.create);
 task.get("/", TaskController.find);
 task.get("/:id", TaskController.findOne);
-task.put("/:id", TaskController.update);
+task.patch("/:id", TaskController.update);
 task.delete("/:id", TaskController.delete);
 
 module.exports = task;
